c2ab7e6c2fc9042da6b03eeb5750fd1bc5deb67b
Awe Ayomidipupo <pupoawe@gmail.com>
2023-09-06 15:20:53 +0100

    fix

--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,4 @@
 export default function extractBasicCredentials(base64) {
-  const credentials = Buffer.from(base64, 'base64').split(':');
+  const credentials = Buffer.from(base64, 'base64').toString().split(':');
   return { email: credentials[0], password: credentials[1] };
 }
