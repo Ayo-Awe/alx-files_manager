fcd2a44669f4856ceda08372ec6c9be9f933ad11
Awe Ayomidipupo <pupoawe@gmail.com>
2023-09-06 11:36:11 +0100

    feat: setup redis client utils

---
+++ utils/redis.js
@@ -0,0 +1,33 @@
+import { createClient } from "redis";
+import { promisify } from "util";
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on("error", (err) => {
+      console.log(err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get);
+    return getAsync.call(this.client, key);
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.set);
+    await setAsync.call(this.client, key, value, "EX", duration);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del);
+    await delAsync.call(this.client, key);
+  }
+}
+
+export default new RedisClient();
